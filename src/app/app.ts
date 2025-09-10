d7a9fc9f5858f4e4ed2f2ea669429bd16176b2a7
B-oviedo <bnjmnvd@gmail.com>
2025-09-10 18:15:37 -0300

    Agrege el footer y estilos en el footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 // 👇 Importa tu componente header
 import { Header } from './components/header/header';
+import { Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true, // 👈 Esto también es importante
-  imports: [RouterOutlet, Header], // 👈 Agrégalo aquí
+  imports: [RouterOutlet, Header, Footer], // 👈 Agrégalo aquí
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
